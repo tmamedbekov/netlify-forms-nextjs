ee168a46273e1a963a3046fdf0ae21ac486280bb
Tony Mamedbekov <tony.mamedbekov@gmail.com>
2021-05-25 23:17:17 -0500

    few changes

--- pages/form.js
+++ pages/form.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import ContactForm from '../components/contantForm'
+import ContactForm from '../components/ContactForm'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
