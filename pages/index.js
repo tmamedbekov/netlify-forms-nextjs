d416a5ac5dada61d661177a2893d636a74d7fb98
Tony Mamedbekov <tony.mamedbekov@gmail.com>
2021-05-25 22:34:52 -0500

    initial push

--- pages/index.js
+++ pages/index.js
@@ -41,12 +41,12 @@ export default function Home() {
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="/form"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Forms &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Netlify Forms are cool especially with Zappier and Google Sheets
             </p>
           </a>
         </div>
