6fd4ec4e8fe4ca13b5ec1ed3369780887a632374
Tony Mamedbekov <tony.mamedbekov@gmail.com>
2021-05-25 23:38:20 -0500

    fix the form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -3,23 +3,12 @@ export default function ContactForm() {
         <form name="contact" action="/thanks" method="POST" data-netlify="true">
             <input type="hidden" name="form-name" value="contact" />
             <p>
-                <input type="text" name="firstname" id="firstname" />
-                <label htmlFor="yourname">
-                    Your Name:
-          </label> <br />
-                <input type="text" name="name" id="yourname" />
-            </p>
-            <p>
-                <label htmlFor="youremail">
-                    Your Email:
-          </label> <br />
-                <input type="email" name="email" id="youremail" />
-            </p>
-            <p>
-                <label htmlFor="yourmessage">
-                    Message:
-          </label> <br />
-                <textarea name="message" id="yourmessage"></textarea>
+                <label htmlFor="yourname">Your Name:</label>
+                <br />
+                <input type="text" name="name" id="name" />
+                <label htmlFor="youremail">Your Email:</label>
+                <br />
+                <input type="text" name="email" id="email" />
             </p>
             <p>
                 <button type="submit">Send</button>
